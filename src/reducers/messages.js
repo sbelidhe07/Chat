867bddfeb94690d2bc96535540f27d4ce8f70869
sbelidhe07 <44050608+sbelidhe07@users.noreply.github.com>
2021-02-05 18:33:01 +0530

    Add files via upload

---
+++ src/reducers/messages.js
@@ -0,0 +1,18 @@
+const messages = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_MESSAGE':
+    case 'MESSAGE_RECEIVED':
+      return state.concat([
+        {
+          message: action.message,
+          author: action.author,
+          id: action.id,
+          date: action.date
+        }
+      ])
+    default:
+      return state
+  }
+}
+
+export default messages
