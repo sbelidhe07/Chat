8425da1dc5b62ad03a157c3724c5b120b54ed614
sbelidhe07 <44050608+sbelidhe07@users.noreply.github.com>
2021-02-05 18:19:48 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,54 @@
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 5000 })
+
+const users = []
+
+const broadcast = (data, ws) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN && client !== ws) {
+      client.send(JSON.stringify(data))
+    }
+  })
+}
+
+wss.on('connection', (ws) => {
+  let index
+  ws.on('message', (message) => {
+    const data = JSON.parse(message)
+    console.log(message)
+    switch (data.type) {
+      case 'ADD_USER': {
+        index = users.length
+        users.push({ name: 'user'+index, id: index + 1 })
+        ws.send(JSON.stringify({
+          type: 'USERS_LIST',
+          users
+        }))
+        broadcast({
+          type: 'USERS_LIST',
+          users
+        }, ws)
+        break
+      }
+      case 'ADD_MESSAGE':
+        broadcast({
+          type: 'ADD_MESSAGE',
+          message: data.message,
+          author: data.author,
+          date: data.date 
+        }, ws)
+        break
+      default:
+        break
+    }
+  })
+
+  ws.on('close', () => {
+    users.splice(index, 1)
+    broadcast({
+      type: 'USERS_LIST',
+      users
+    }, ws)
+  })
+})
